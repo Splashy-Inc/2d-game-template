8706ad72ecf562893c12632fa5d6339807164336
avelitj12 <avelitj12@gmail.com>
2025-04-21 18:20:40 -0400

    Add example mobile controls

--- SCRIPTS/level.gd
+++ SCRIPTS/level.gd
@@ -2,13 +2,22 @@ extends Node
 
 class_name Level
 
+@export var dialog_box: DialogBox
+@export var mobile_controls: MobileControls
+
 func _ready() -> void:
-	for child in get_children():
-		if child is DialogBox:
-			child.dialog_ended.connect(_on_dialog_ended.bind(child))
+	if dialog_box:
+		dialog_box.dialog_ended.connect(_on_dialog_ended.bind(dialog_box))
+	if mobile_controls:
+		if OS.has_feature("web_android") or OS.has_feature("web_ios") or OS.has_feature("mobile"):
+			mobile_controls.show()
+		else:
+			mobile_controls.hide()
 
 func _process(delta: float) -> void:
-	pass
+	# Ensure that the mobile controls are not visible if the dialog box is present
+	if mobile_controls.visible and dialog_box.visible:
+		mobile_controls.hide()
 
 func _physics_process(delta: float) -> void:
 	pass
